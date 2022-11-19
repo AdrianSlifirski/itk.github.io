443da4f56b71afbc3229e09f8752c39841fa2331
AdrianSlifirski <83457924+AdrianSlifirski@users.noreply.github.com>
2022-11-19 18:33:24 +0100

    Added main.js file with some indexedDB implementation.

---
+++ lab4/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	var request = indexedDB.open('customermanager',1);
+	
+	request.onupgradeneeded = function(e){
+		var db = e.target.result;
+		
+		if(!db.objectStoreNames.contains('customers')){
+			var os = db.createObjectStore('customers',{keyPath: "id", autoIncrement:true});
+			os.createIndex('name','name',{unique:false});
+		}
+	}
+	request.onsuccess = function(e){
+		console.log('Success: Opened Database...');
+		db = e.target.result;
+		showCustomers();
+	}
+	request.onerror = function(e){
+		console.log('Error: Could Not Open Database...');
+	}
+});
