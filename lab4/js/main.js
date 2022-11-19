2c594314c4455c8bad3dec6a2c4ec9d955785171
AdrianSlifirski <83457924+AdrianSlifirski@users.noreply.github.com>
2022-11-19 19:08:50 +0100

    Added rest of functions to main.js file.

--- lab4/js/main.js
+++ lab4/js/main.js
@@ -1,21 +1,19 @@
 $(document).ready(function(){
 	var request = indexedDB.open('customermanager',1);
-	
 	request.onupgradeneeded = function(e){
 		var db = e.target.result;
-		
 		if(!db.objectStoreNames.contains('customers')){
 			var os = db.createObjectStore('customers',{keyPath: "id", autoIncrement:true});
 			os.createIndex('name','name',{unique:false});
 		}
 	}
 	request.onsuccess = function(e){
-		console.log('Success: Opened Database...');
+		console.log('Success: Opened Database');
 		db = e.target.result;
 		showCustomers();
 	}
 	request.onerror = function(e){
-		console.log('Error: Could Not Open Database...');
+		console.log('Error: Could Not Open Database');
 	}
 });
 
@@ -46,4 +44,90 @@ function showCustomers(e){
 function deleteCustomers(){
 	indexedDB.deleteDatabase('customermanager');
 	window.location.href="index.html";
+}
+
+function removeCustomer(id){
+	var transaction = db.transaction(["customers"],"readwrite");
+	var store = transaction.objectStore("customers");
+	var request = store.delete(id);
+	request.onsuccess = function(){
+		console.log('customer '+id+' Deleted');
+		$('.customer_'+id).remove();
+	}
+	request.onerror = function(e){
+		console.log('Error', e.target.error.name);
+	}
+}
+
+$('#customers').on('blur','.customer',function(){
+	var newText = $(this).html();
+	var field = $(this).data('field');
+	var id = $(this).data('id');
+	var transaction = db.transaction(["customers"],"readwrite");
+	var store = transaction.objectStore("customers");
+	var request = store.get(id);
+	request.onsuccess = function(){
+		var data = request.result;
+		if(field == 'name'){
+			data.name = newText;
+		}
+		else if(field == 'city'){
+			data.city = newText;
+		} 
+		else if(field == 'adress'){
+			data.adress = newText;
+		}
+		else if(field == 'email'){
+			data.email = newText;
+		}
+		else if(field == 'postal_code'){
+			data.postal_code = newText;
+		}
+		else if(field == 'nip_number'){
+			data.nip_number = newText;
+		}
+		else if(field == 'id_card'){
+			data.id_card = newText;
+		}
+		var requestUpdate = store.put(data);
+		requestUpdate.onsuccess = function(){
+			console.log('Customer field updated...');
+		}
+		requestUpdate.onerror = function(){
+			console.log('Error: Customer field NOT updated...');
+		}
+	}
+	request.onerror = function(e){
+		console.log('Error test');
+	}
+});
+
+function addCustomer(){
+	var name = $('#name').val();
+	var city = $('#city').val();
+	var adress = $('#adress').val();
+	var email = $('#email').val();
+	var postal_code = $('#postal_code').val();
+	var nip_number = $('#nip_number').val();
+	var phone_number = $('#phone_number').val();
+	var id_card = $('#id_card').val();
+	var transaction = db.transaction(["customers"],"readwrite");
+	var store = transaction.objectStore("customers");
+	var customer = {
+		name: name,
+		city: city,
+		adress: adress,
+		email: email,
+		postal_code: postal_code,
+		nip_number: nip_number,
+		phone_number: phone_number,
+		id_card: id_card,
+	}
+	var request = store.add(customer);
+	request.onsuccess = function(e){
+		window.location.href="index.html";
+	}
+	request.onerror = function(e){
+		console.log('Error the customer was not added', e.target.error.name);
+	}
 }
